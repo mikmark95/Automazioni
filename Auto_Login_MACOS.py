702ac46a66a83c8be3b02931fa0e9e63bcf2f327
mikmark95 <michele-marchetti@hotmail.it>
2023-06-30 23:32:27 +0200

    aggiunte del Service

---
+++ Auto_Login_MACOS.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+path = 'C:/Users/emmes/Documents/Python Scripts/chromedriver.exe'
+service = Service(executable_path=path)
+def get_driver(url):
+    '''
+    Funzione che imposta i driver per la connessione con la speficia pagiva web
+    :return: driver
+    '''
+    #Imposto le opzioni del browser
+    options = webdriver.ChromeOptions()
+    #Disalibito la barra delle informazioni, poichè potrebbe interverire con gli script
+    options.add_argument("disable-infobars")
+    #Faccio in modo che la finestra del browser sia massimizzata
+    options.add_argument("start-maximized")
+    #Serve per prevenire problemi che si verificano qunado interagisi con un browser su linux
+    options.add_argument("disable-dev-shm-usage")
+    #Serve per disabilitare le SandBox del browser
+    options.add_argument("no-sandbox")
+    #Serve per abilitare i nostri script sui browser
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+
+    #Driver per utilizzare il browser Google Chrome
+    driver = webdriver.Chrome( service=service,options=options )
+    #Aggiungo l'url della pagina web da raggiungere
+    driver.get(url)
+
+    return driver
+
+
+def clean_text(stringa:str):
+    '''
+    Funzione che isola la parte di testo che ci interessa
+    :param stringa:
+    :return:
+    '''
+
+    l = stringa.split(':')
+    return l[1]
+
+
+def cattura_testo(ripetizioni:int, driver, output):
+    '''
+    Cattura il testo e salva il contenuto in un file txt, viene eseguito un numero di volte pari a ripetizioni
+    :param ripetizioni: numero iterazioni
+    :param driver: driver per leggere il sito
+    :param output: percorso di output dei file
+    :return:
+    '''
+    for x in range(ripetizioni):
+        output = output+f"{x}"
+        time.sleep(2)
+        text = driver.find_element(by='id', value="displaytimer")
+        t = clean_text(text.text)
+        with open(output, mode='w',encoding='UTF-8') as out:
+            out.write(t)
+    return f'FILE CREATI !\nDisponibili in {output}'
+
+
+def main():
+    driver = get_driver("https://automated.pythonanywhere.com/login/")
+    #Estraiamo un elemento dal driver tramite la ricerca tramite il xpath
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated")
+    driver.find_element(by='xpath', value="/html/body/div[1]/div/div/div[3]/form/button").click()
+    driver.find_element(by='xpath', value="/html/body/nav/div/a").click()
+    time.sleep(2)
+    text = driver.find_element(by='id', value="displaytimer")
+    cattura_testo(3,driver,"/Users/michelemarchetti/Downloads/")
+
+    return clean_text(text.text)
+
+
+if __name__ == '__main__':
+    print(main())
